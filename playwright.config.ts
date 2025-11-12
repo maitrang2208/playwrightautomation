b87a2ddee22388a88efd3116dfefc8701c83b469
maitrang2208 <maithithutrang2208@gmail.com>
2025-11-12 14:23:44 +0700

    add code bai 10 crm

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,6 +27,8 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
+    actionTimeout:10000,
+    navigationTimeout:30000,
     headless: false,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
